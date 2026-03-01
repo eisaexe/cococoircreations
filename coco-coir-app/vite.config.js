a50a80b6fec9a232a13597ad809d7c7802eb8399
Mohammed Eisa <141444285+eisaexe@users.noreply.github.com>
2026-03-01 19:10:01 +0530

    fixed vite

--- coco-coir-app/vite.config.js
+++ coco-coir-app/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',   // <-- add this line
+  base: '/',   // IMPORTANT: use '/' for Vercel
   server: {
     allowedHosts: true
   }
